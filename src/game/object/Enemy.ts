db5f76301e9508e3a74084d4d8f9738b78448f67
Brian Ingles <github@emeraldwalk.com>
2024-12-06 23:05:25 -0600

    Using collideWithObject for enemy

--- src/game/object/Enemy.ts
+++ src/game/object/Enemy.ts
@@ -1,21 +1,19 @@
-import { Color, engineObjects, vec2, type Vector2 } from 'littlejsengine'
+import { Color, vec2, type EngineObject } from 'littlejsengine'
 import type { AnimationID, EntityData, IEnemy } from '../model'
 import { Character } from './Character'
-import { isColliding, isPlayer } from '../util'
+import { isPlayer } from '../util'
 
 export class Enemy extends Character implements IEnemy {
   constructor(entity: EntityData, animation?: AnimationID) {
     super('enemy', entity, animation, new Color(1, 0, 0))
   }
 
-  update(): void {
-    super.update()
-
-    const player = engineObjects.find(isPlayer)
-
-    if (player && isColliding(this, player)) {
+  collideWithObject(object: EngineObject): boolean {
+    if (isPlayer(object)) {
       const force = vec2().setAngle(this.angle, this.attack)
-      player.damage(force)
+      object.damage(force)
     }
+
+    return super.collideWithObject(object)
   }
 }
